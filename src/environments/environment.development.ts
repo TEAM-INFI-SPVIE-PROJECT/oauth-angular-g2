f8f07cce101e60e2a1452b6c67ca189012713248
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2024-02-01 16:01:11 +0000

    Small changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,8 +4,7 @@ const oAuthConfing: AuthConfig = {
   issuer: 'https://accounts.google.com',
   strictDiscoveryDocumentValidation: false,
   redirectUri: window.location.origin,
-  clientId:
-    '16317450381-7aeki2toahkuh8pgv2tmg93t84ds4pbn.apps.googleusercontent.com',
+  clientId: 'Add your client ID',
   scope: 'openid profile email https://www.googleapis.com/auth/gmail.readonly',
 };
 
